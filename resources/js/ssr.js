f5462dded0a8aa27a49863ac0b7c2197de595675
Kafri <kafri@Prevalensis-MacBook-Air.local>
2024-03-28 09:57:40 +0700

    config component

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,6 +1,7 @@
 import { createInertiaApp } from '@inertiajs/react';
 import createServer from '@inertiajs/react/server';
 import ReactDOMServer from 'react-dom/server';
+import { hydrateRoot } from 'react-dom/client';
 
 createServer((page) =>
     createInertiaApp({
@@ -10,6 +11,10 @@ createServer((page) =>
             const pages = import.meta.glob('./pages/**/*.jsx', { eager: true });
             return pages[`./pages/${name}.jsx`];
         },
-        setup: ({ App, props }) => <App {...props} />,
+        // setup: ({ App, props }) => <App {...props} />,
+        setup({ el, App, props }) {
+            // -     createRoot(el).render(<App {...props} />)
+            hydrateRoot(el, <App {...props} />);
+        },
     }),
 );
